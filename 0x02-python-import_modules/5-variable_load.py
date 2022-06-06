ba6dae7b6fb0fbf31b86704c512448b9f7be781d
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-06-06 21:54:02 +0200

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Imports the variable a from the file variable_load_5.py and prints its value."""
+    """Imports the variable a from the file and prints its value."""
     from variable_load_5 import a
 
     print(a)
