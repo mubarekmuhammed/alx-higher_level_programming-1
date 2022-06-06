404575a3d4aae34fa8260372cc5288088fe2cb9f
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-06-06 21:46:44 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     """Prints all the names defined by the compiled module hidden_4.pyc"""
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2) != "__":
-            print(name)
+     mylist = dir(hidden_4)
+    newlist = sorted(mylist)
+    while i < len(newlist):
+        if newlist[i][0] != '_':
+            print(newlist[i])
+        i += 1
