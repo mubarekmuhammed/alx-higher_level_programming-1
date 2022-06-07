4b0e5af983ae1f3c211b54a7d70735ca315712fa
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-06-08 01:54:18 +0200

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    if num < 10:
-        print("0{:d}".format(num), end=", ")
-    elif num >= 10 and num < 98:
-        print("{:d}".format(num), end=", ")
-    else:
-        print("{:d}".format(num))
+for num in range(00, 98):
+    print("{:02d}".format(num), end='\n' if num == 98 else ", ")
