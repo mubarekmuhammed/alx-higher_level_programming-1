c242de986db6c6ceb7eb91cfbabca737af29da72
root <delanolesage@gmail.com>
2022-06-07 12:24:11 -0700

    Create 3-print_reversed_list_integer.py

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr//bin/python3
+def print_reversed_list_integer(my_list=[]):
+    l = len(my_list) - 1
+
+    while l > 0:
+        print("{}\n".format(my_list[l])
+        l--
+        if l == 0:
+            print("{}".format(my_list[l]
