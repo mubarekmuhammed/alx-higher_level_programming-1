ae3b077add5a99dd7a7a7c808af98f2e8c761c82
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-06-12 02:29:28 +0200

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for char in range(97, 123, -1):
-	if char % 2 == 0:
-		print('{:c}'.format(char - 32), end='')
-	print('{:c}'.format(char), end='')
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        i = i - 32
+    print("{}".format(chr(i)), end="")
