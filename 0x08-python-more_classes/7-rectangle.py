cc1033f52a165700b7a39ee187305cda111f1e43
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-06-29 00:55:52 +0200

    Update 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,7 +6,6 @@ Defines a Rectangle class.
 
 class Rectangle:
     """Rectangle class defined by width and height.
-
     Attributes:
         number_of_instances: number of Rectangle instances,
         increments with every instantitation,
@@ -14,6 +13,7 @@ class Rectangle:
     """
 
     number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle instance.
@@ -27,7 +27,7 @@ class Rectangle:
 
     def __str__(self):
         """Returns an informal and nicely printable string representation
-        of a Rectangle instance, filled with the chosen string."""
+        of a Rectangle instance, filled with the chosen pattern."""
         if self.__height == 0 or self.__width == 0:
             return ''
         rec_str = ''
